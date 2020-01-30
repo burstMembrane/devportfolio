6e9a3ac03429efb404610c286b610dd47dc0cb01
Liam <liamfpower@gmail.com>
2020-01-30 21:01:11 +1100

    changed about page

--- refreshscript.sh
+++
@@ -1,3 +0,0 @@
-# this script starts a nodemon server that watches for file changes, and also auto reloads the browser on file change 
-sudo killall node
-nodemon -e js,css,ejs & watchme . views web -e "osascript /users/liampower/refresh.applescript" -q
